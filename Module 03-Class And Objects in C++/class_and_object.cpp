756991af9df8c47d5f0c7d4c88f7ea2622fab573
amitsengupta <amitsengupta332@gmail.com>
2025-05-27 21:24:20 +0600

    class and object

---
+++ Module 03-Class And Objects in C++/class_and_object.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+
+    public:
+    char name[100];
+    int roll;
+    double gpa;
+};
+
+int main()
+{
+    Student a;
+    a.roll = 10;
+    a.gpa = 3.5;
+    char temp[100] = "Amit";
+    strcpy(a.name, temp);  
+
+    cout << a.name << " " << a.roll << " " << a.gpa << endl;
+  
+    return 0;
+}
