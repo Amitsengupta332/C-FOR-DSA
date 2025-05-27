3b11d31394317bd12093063b16ce1249210e3199
amitsengupta <amitsengupta332@gmail.com>
2025-05-28 00:57:35 +0600

    dynamc object

---
+++ Module 03-Class And Objects in C++/dynamic_object.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa)
+    {
+
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+int main()
+{
+    Student rahim(50, 10, 3.75);
+    Student *karim = new Student(1, 10, 5.00); // This creates a dynamic object but does not store it anywhere
+
+    // Student karim(1, 10, 5.00);
+    cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+
+    cout << karim->roll << " " << karim->cls << " " << karim->gpa << endl;
+
+    return 0;
+}
