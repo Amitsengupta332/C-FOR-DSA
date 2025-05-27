8e2692fe7dcf511b99c1820b3dcfa92811ad0681
amitsengupta <amitsengupta332@gmail.com>
2025-05-28 01:02:30 +0600

    dynamic object return from function

---
+++ Module 03-Class And Objects in C++/dynamic_object_return_from_function.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa)
+    {
+
+        // (*this).roll = roll;
+        // (*this).cls = cls;
+        // (*this).gpa = gpa;
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+Student* fun()
+{
+    Student* karim = new Student(1, 10, 5.00);
+    // Student *p = &karim;
+    return karim; // This will cause a compilation error
+}
+
+int main()
+{
+    // Student rahim(50, 10, 3.75);
+    Student* p = fun();
+    // Student karim(1, 10, 5.00);
+
+    // cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+    cout << p->roll << " " << p->cls << " " << p->gpa << endl;
+    // cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+
+    return 0;
+}
