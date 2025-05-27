69a102717eb90ed4c6166bc59be8edd75d78e6fc
amitsengupta <amitsengupta332@gmail.com>
2025-05-28 01:20:47 +0600

    build in sorting

---
+++ Module 03-Class And Objects in C++/sorting.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        /* code */
+    }
+
+    // sort(starting, ending);
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+        /* code */
+    }
+    
+    
+
+
+  
+    return 0;
+}
