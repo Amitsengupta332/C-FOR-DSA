cd39c95d693e13d355390a185a8ed12c11172046
amitsengupta <amitsengupta332@gmail.com>
2025-05-28 00:41:27 +0600

    g

---
+++ Module 03-Class And Objects in C++/why_we_need_dynamic_object.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa)
+    {
+
+        // (*this).roll = roll;
+        // (*this).cls = cls;
+        // (*this).gpa = gpa;
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+Student fun()
+{
+    Student karim(1, 10, 5.00);
+    return karim; // This will cause a compilation error
+}
+
+int main()
+{
+    // Student rahim(50, 10, 3.75);
+    Student obj = fun();
+    // Student karim(1, 10, 5.00);
+
+    // cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+    cout << obj.roll << " " << obj.cls << " " << obj.gpa << endl;
+    // cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+
+    return 0;
+}
