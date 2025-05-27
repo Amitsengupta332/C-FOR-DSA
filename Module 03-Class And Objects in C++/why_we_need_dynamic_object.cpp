62382b0acd2ff561383968fa93c692e423ec79d2
amitsengupta <amitsengupta332@gmail.com>
2025-05-28 00:48:03 +0600

    why we need dynamic object

--- Module 03-Class And Objects in C++/why_we_need_dynamic_object.cpp
+++ Module 03-Class And Objects in C++/why_we_need_dynamic_object.cpp
@@ -20,20 +20,21 @@ public:
     }
 };
 
-Student fun()
+Student* fun()
 {
     Student karim(1, 10, 5.00);
-    return karim; // This will cause a compilation error
+    Student *p = &karim;
+    return p; // This will cause a compilation error
 }
 
 int main()
 {
     // Student rahim(50, 10, 3.75);
-    Student obj = fun();
+    Student* p = fun();
     // Student karim(1, 10, 5.00);
 
     // cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
-    cout << obj.roll << " " << obj.cls << " " << obj.gpa << endl;
+    cout << p->roll << " " << p->cls << " " << p->gpa << endl;
     // cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
 
     return 0;
