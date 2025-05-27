2ca817195e200ad49336c219afa8ff81e1e3b546
amitsengupta <amitsengupta332@gmail.com>
2025-05-27 21:36:49 +0600

    class and object user input

--- Module 03-Class And Objects in C++/class_and_object.cpp
+++ Module 03-Class And Objects in C++/class_and_object.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Student
 {
 
-    public:
+public:
     char name[100];
     int roll;
     double gpa;
@@ -12,13 +12,27 @@ class Student
 
 int main()
 {
-    Student a;
-    a.roll = 10;
-    a.gpa = 3.5;
-    char temp[100] = "Amit";
-    strcpy(a.name, temp);  
+    Student a, b;
+
+    cin.getline(a.name, 100);
+    cin >> a.roll >> a.gpa;
+
+    // cin.ignore(); // To ignore the newline character left in the input buffer
+    getchar(); // To consume the newline character before reading the next line
+    cin.getline(b.name, 100);
+    cin >> b.roll >>b.gpa;
+    // cin >> a.name >> a.roll >> a.gpa;
+    // cin >> b.name >> b.roll >> b.gpa; Tamal Chakraborty 03 3.5
 
     cout << a.name << " " << a.roll << " " << a.gpa << endl;
-  
+    cout << b.name << " " << b.roll << " " << b.gpa << endl;
+
+    // a.roll = 10;
+    // a.gpa = 3.5;
+    // char temp[100] = "Amit";
+    // strcpy(a.name, temp);
+
+    // cout << a.name << " " << a.roll << " " << a.gpa << endl;
+
     return 0;
 }
