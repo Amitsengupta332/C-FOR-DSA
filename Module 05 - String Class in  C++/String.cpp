94cd1427302fa1d4968f1a2003fb139fdd579a45
amitsengupta <amitsengupta332@gmail.com>
2025-05-31 22:00:18 +0600

    string in c

---
+++ Module 05 - String Class in  C++/String.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // char s[10] = "Hello";
+    // strcpy(s, "Gello");
+    // string s = "Hello";
+    // s= "Gello";
+
+    string s = "Hello";
+    string s2 = "Hello";
+    if(s == s2)
+    {
+        cout << "Same" << endl;
+    }
+    else
+    {
+        cout << "Not same" << endl;
+    }
+    // cout << s << endl;
+    return 0;
+}
