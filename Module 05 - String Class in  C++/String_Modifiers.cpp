22f11671fae79b9803fae26fe69955c7a20157aa
amitsengupta <amitsengupta332@gmail.com>
2025-06-01 01:30:51 +0600

    string modifiers part i

---
+++ Module 05 - String Class in  C++/String_Modifiers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello World";
+    // string s1 = "HI";
+    // s+= s1;  
+    // s.append(s1); // Append s1 to s
+    s.push_back('A');  
+    cout << s << endl; // Output the concatenated string
+    return 0;
+}
