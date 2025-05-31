06f2aa09ce77d2524ee202ba40b5e6436f5056da
amitsengupta <amitsengupta332@gmail.com>
2025-06-01 00:20:46 +0600

    string capacity

---
+++ Module 05 - String Class in  C++/String_Capacity.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello World";
+    // s.resize(4);
+    s.resize(15, 'zzzz');
+    // s.clear();
+
+    // if(s.empty() == true)
+    // {
+    //     cout << "String is empty" << endl;
+    // }
+    // else
+    // {
+    //     cout << "String is not empty" << endl;
+    // }
+
+    // cout << s.size() << endl;     // Output the size of the string
+    // cout << s.max_size() << endl; // Output the maximum size of the string
+    // cout << s.capacity() << endl; // Output the current capacity of the string
+    // cout << s.clear() << endl;    // Clear the string, making it empty
+    cout << s << endl; // Output the string
+    return 0;
+}
