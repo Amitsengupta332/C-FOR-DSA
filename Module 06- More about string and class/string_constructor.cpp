2aaf10b9d1297ce5b89b9c138bfbf87ee96ca4a7
amitsengupta <amitsengupta332@gmail.com>
2025-06-01 22:12:15 +0600

    string_constructor

---
+++ Module 06- More about string and class/string_constructor.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //    string s = "Hello";
+
+    // 1
+    //    string s("Hello");
+    // 2
+    //    string s("Hello", 4);
+
+    // string s = "Hello, World!";
+    // string t(s, 4);
+    string s(5, 'A');
+    // cout << t << endl;
+    cout << s << endl;
+    return 0;
+}
