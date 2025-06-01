3315d21c120cc3718874e613d2b429d6fb3bab6c
amitsengupta <amitsengupta332@gmail.com>
2025-06-02 01:18:32 +0600

    reversed function

--- Module 06- More about string and class/Reversed_function.cpp
+++ Module 06- More about string and class/Reversed_function.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
+    // int n;
+    // cin >> n;
+    // int a[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> a[i];
+    // }
+
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+    cout << s << endl;
     // sort();
-    reverse(a, a + n);
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << " ";
-    }
+    // reverse(a, a + n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << a[i] << " ";
+    // }
     return 0;
 }
