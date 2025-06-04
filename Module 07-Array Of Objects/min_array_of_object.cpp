33b4f420cde793fa7e5a706bac1003aa3eda3f1b
amitsengupta <amitsengupta332@gmail.com>
2025-06-04 15:15:17 +0600

    min array of object

---
+++ Module 07-Array Of Objects/min_array_of_object.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    Student a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+    // print whole object
+    Student mn;
+    mn.marks = INT_MIN;
+    // mn.marks = INT_MAX;
+    // int mn = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i].marks > mn.marks)
+        {
+            mn = a[i];
+        }
+        // if (a[i].marks < mn.marks)
+        // {
+        //     mn = a[i];
+        // }
+        // mn = min(a[i].marks, mn.marks);
+        // mn = min(a[i].marks, mn);
+        // cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    }
+    // cout << mn << endl;
+    cout << mn.name << " " << mn.roll << " " << mn.marks << endl;
+                                        
+    return 0;
+}
