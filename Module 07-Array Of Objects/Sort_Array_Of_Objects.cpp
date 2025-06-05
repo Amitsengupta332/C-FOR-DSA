e8b8c28ab38659008d8b4481b5665e65bd6b3d24
amitsengupta <amitsengupta332@gmail.com>
2025-06-05 21:49:16 +0600

    sort array

--- Module 07-Array Of Objects/Sort_Array_Of_Objects.cpp
+++ Module 07-Array Of Objects/Sort_Array_Of_Objects.cpp
@@ -11,14 +11,47 @@ public:
 
 bool cmp(Student l, Student r)
 {
-    if (l.marks < r.marks)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    // if (l.marks < r.marks)
+    // {
+    //     return true;
+    // }
+    // else
+    // {
+    //     return false;
+    // }
+
+    // ternary operator
+    //    ( l.marks == r.marks) ? return l.roll < r.roll : return l.marks > r.marks;
+    return (l.marks == r.marks) ? l.roll < r.roll : l.marks > r.marks;
+
+    // if (l.marks == r.marks)
+    // {
+    //     return l.roll < r.roll; // If marks are equal, sort by roll number
+    // }else{
+    //     return l.marks > r.marks; // Sort by marks in descending order
+    // }
+
+    // if (l.marks > r.marks)
+    // {
+    //     return true;
+    // }
+    // else if( l.marks < r.marks)
+    // {
+    //     return false;
+    // }
+    // else{
+    //     return l.roll < r.roll; // If marks are equal, sort by roll number
+
+    //     // if (l.roll < r.roll)
+    //     // {
+    //     //     return true;
+    //     //     /* code */
+    //     // }
+    //     // else{
+    //     //     return false;
+    //     // }
+
+    // }
 }
 int main()
 {
