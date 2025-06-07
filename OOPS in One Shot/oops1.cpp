f03678802f00b9956585a626022f73a7aa9c305e
amitsengupta <amitsengupta332@gmail.com>
2025-06-07 21:20:25 +0600

    class and object

--- OOPS in One Shot/oops1.cpp
+++ OOPS in One Shot/oops1.cpp
@@ -6,8 +6,26 @@ using namespace std;
 //  int data_2;
 // };
 
+class Fruit
+{
+
+public: // Access specifier
+    string name;
+    string color;
+};
+
 int main()
 {
-  
+    Fruit apple; // Object
+    apple.name = "Apple";
+    apple.color = "Red";
+    cout << apple.name << "-" << apple.color << endl;
+
+    // pointer
+    Fruit *mango = new Fruit;
+    mango->name = "Mango";
+    mango->color = "Yellow";
+    cout << mango->name << "-" << mango->color << endl;
+
     return 0;
 }
